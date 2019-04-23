f6f12e2e168830e6d2802f671a5e3b14ff8850da
Tung Nguyen <tungkiet@gmail.com>
2019-04-24 04:41:35 +0700

    2404-0441
    2404-0441

--- js/functions.js
+++ js/functions.js
@@ -1,34 +1,55 @@
-// Browser detection for when you get desparate. A measure of last resort.
-// http://rog.ie/post/9089341529/html5boilerplatejs
-
-// var b = document.documentElement;
-// b.setAttribute('data-useragent',  navigator.userAgent);
-// b.setAttribute('data-platform', navigator.platform);
-
-// sample CSS: html[data-useragent*='Chrome/13.0'] { ... }
-
-
-// remap jQuery to $
 (function($){
+    /*var: top news notice*/
+    var slTopNews = {
+        init: function () {
+            slTopNews.events();
+        },
+        events: function () {
+            $('.slTopNews').owlCarousel({
+                items: 1,
+                dots: false,
+                loop: true,
+                autoplay: true
+            })
+        }
+    };
 
-	/* trigger when page is ready */
+    /*var: banner*/
+    var slBanner = {
+        init: function () {
+            slBanner.events();
+        },
+        events: function () {
+            $('.slBanner').owlCarousel({
+                items: 1,
+                dots: false,
+                loop: true,
+                autoplay: true,
+                margin: 3,
+                autoplaySpeed: 2000
+            })
+        }
+    };
+
 	$(document).ready(function (){
-	
-		// your functions go here
-	
-	});
-	
-	
-	/* optional triggers
-	
-	$(window).load(function() {
-		
-	});
-	
-	$(window).resize(function() {
-		
-	});
-	
-	*/
+        /*call: top news notice*/
+        if ($(".slTopNews").length > 0) {
+            slTopNews.init();
+        }
 
+        /*call: banner*/
+        if ($(".slBanner").length > 0) {
+            slBanner.init();
+        }
+
+
+        /*event click select item search*/
+        $(document).on('click','.tbe-search .dropdown-menu li a',function () {
+            var iSrcbtn = $('.tbe-search .btn img').attr('src');
+            var iSrcSec = $(this).find($('.tbe-search .dropdown-menu img')).attr('src');
+            $('.tbe-search .btn img').attr('src',iSrcSec);
+            $(this).find($('.tbe-search .dropdown-menu img')).attr('src',iSrcbtn);
+        });
+	
+	});
 })(window.jQuery);
